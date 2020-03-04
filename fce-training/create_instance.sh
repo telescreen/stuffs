207cd17ea3bd71e8f003be320200e7610f7c57fb
telescreen <ha@buihanotes.com>
2020-03-04 11:06:05 +0900

    Update interface scripts. Add tag_instance script.

--- fce-training/create_instance.sh
+++ fce-training/create_instance.sh
@@ -7,19 +7,16 @@ SSH_USER=telescreen
 MAAS_SERVER="172.16.224.3"
 
 # Default values for argument variables
-TEMPLATE_NAME="node"
-NODE_NUM=1
+NAME="node"
 CPU=1
 MEM=1024
 OS="ubuntu18.04"
 NUMBER_DATA_DISK=0
 DATA_DISK_SIZE=10G
 
-while getopts ":t:n:c:m:d:s:" opt; do
+while getopts ":n:c:m:d:s:" opt; do
   case ${opt} in
-    t) TEMPLATE_NAME="$OPTARG"
-    ;;
-    n) NODE_NUM="$OPTARG"
+    n) NAME="$OPTARG"
     ;;
     c) CPU="$OPTARG"
     ;;
@@ -29,10 +26,9 @@ while getopts ":t:n:c:m:d:s:" opt; do
     ;;
     s) DATA_DISK_SIZE="$OPTARG"
     ;;
-    \?) echo "create_instance.sh -t TEMPLATE_NAME -n NUMBER_OF_NODE -c CPU_CORE -m MEMORY -d NUMBER_DATADISK -s DATA_DISK_SIZE"
+    \?) echo "create_instance.sh -n NAME -c CPU_CORE -m MEMORY -d NUMBER_DATADISK -s DATA_DISK_SIZE"
 	echo "Default value: "
-	echo "  TEMPLATE_NAME = node"
-	echo "  NUMBER_OF_NODE = 1"
+	echo "  NAME = node"
 	echo "  CPU_CORE = 1"
 	echo "  MEMORY = 1024"
 	echo "  NUMBER_DATA_DISK = 0"
@@ -42,37 +38,35 @@ while getopts ":t:n:c:m:d:s:" opt; do
     esac
 done
 
-for ((NODEID=0; NODEID < $NODE_NUM; NODEID++)); do
 
 # Add data disks	
 for ((DD = 0; DD < $NUMBER_DATA_DISK; DD++)); do
-  qemu-img create -f qcow2 /vms/${TEMPLATE_NAME}${NODEID}-data${DD}.qcow2 $DATA_DISK_SIZE;
+  qemu-img create -f qcow2 /vms/${NAME}-data${DD}.qcow2 $DATA_DISK_SIZE;
 done
 
 MAC_ADDR=`date | md5sum | sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/'`
 
 # Create datadisk
-qemu-img create -f qcow2 /vms/${TEMPLATE_NAME}${NODEID}-root.qcow2 15G
+qemu-img create -f qcow2 /vms/${NAME}-root.qcow2 15G
 
 # Create Instance
 virt-install \
 --connect=qemu:///system \
 --boot network,hd,menu=on \
---name ${TEMPLATE_NAME}${NODEID} \
+--name ${NAME} \
 --ram ${MEM} \
 --vcpus=${CPU} \
 --cpu host-model \
 --os-variant ${OS} \
 --controller=scsi,model=virtio-scsi \
---disk path=/vms/${TEMPLATE_NAME}${NODEID}-root.qcow2,bus=virtio \
-$(for ((DD = 0; DD < $NUMBER_DATA_DISK; DD++)); do echo --disk path=/vms/${TEMPLATE_NAME}${NODEID}-data${DD}.qcow2,bus=virtio; done) \
+--disk path=/vms/${NAME}-root.qcow2,bus=virtio \
+$(for ((DD = 0; DD < $NUMBER_DATA_DISK; DD++)); do echo --disk path=/vms/${NAME}-data${DD}.qcow2,bus=virtio; done) \
 --import \
 --network type=bridge,source=${NET},virtualport_type=openvswitch,model=virtio,mac=${MAC_ADDR} \
 --sound none \
 --graphics vnc \
 --noautoconsole; \
-virsh destroy ${TEMPLATE_NAME}${NODEID}
+virsh destroy ${NAME}
 
-maas $MAAS_USER machines create hostname=${TEMPLATE_NAME}${NODEID} architecture=amd64/generic mac_addresses=$MAC_ADDR power_type=virsh power_parameters="{\"power_address\": \"qemu+ssh://$SSH_USER@$MAAS_SERVER/system\", \"power_id\": \"${TEMPLATE_NAME}${NODEID}\"}"
-done
+maas $MAAS_USER machines create hostname=${NAME} architecture=amd64/generic mac_addresses=$MAC_ADDR power_type=virsh power_parameters="{\"power_address\": \"qemu+ssh://$SSH_USER@$MAAS_SERVER/system\", \"power_id\": \"${NAME}\"}"
 
--- fce-training/create_interface.sh
+++ fce-training/create_interface.sh
@@ -3,34 +3,29 @@
 # Variable that need to set before running this script
 MAAS_USER=telescreen
 
-while getopts ":t:n:" opt; do
+while getopts ":t:n:v:" opt; do
   case ${opt} in
-    t) NAME="$OPTARG"
+    n) NAME="$OPTARG"
     ;;
-    n) NUMNODES="$OPTARG"
+    t) INF="$OPTARG"
     ;;
-    \?) echo "create_interfaces.sh -t NAME -n NUMBER_OF_NODES"
-        echo "  Create a new interface to virtual machine NAME."
+    v) VLANID="$OPTARG"
+    ;;
+    \?) echo "create_interfaces.sh -n NAME -t NAME_OF_PARENT_INTERFACE -v VLANID"
+        echo "  Create a new interface to virtual machine NAME. Attach VLANID to PARENT_INTERFACE"
 	exit 1;
     ;;
     esac
 done
 
-subnet_ids=`maas $MAAS_USER subnets read | jq '.[] | {name: .name, subnet_id: .id, vlan_id: .vlan.id, vid: .vlan.vid}' --compact-output`
+subnet_id=`maas $MAAS_USER subnets read | jq --argjson VLANID "$VLANID" '.[] | select(.vlan.vid==$VLANID) | {name: .name, subnet_id: .id, vlan_id: .vlan.id}' --compact-output`
+vlan_id=`echo $subnet_id | jq '.vlan_id'`
+subnet_id=`echo $subnet_id | jq '.subnet_id'`
 
-for ((IDX=0; IDX < $NUMNODES; IDX++ )); do
-  system_id=$(maas telescreen machines read | jq --arg name ${NAME}${IDX} '.[] | select(.hostname==$name) | .system_id' | tr -d '"')
-  parent_interface_id=$(maas $MAAS_USER interfaces read $system_id | jq '.[] | .id')
-  for subnet_id in $subnet_ids; do
-    vid=`echo $subnet_id | jq '.vid'`
-    vlan_id=`echo $subnet_id | jq '.vlan_id'`
-    subnet_id=`echo $subnet_id | jq '.subnet_id'`
-    if [ $vid -ne 0 ]; then
-      ifaces=$(maas $MAAS_USER interfaces create-vlan $system_id vlan=$vlan_id parent=$parent_interface_id)
-      tiface=$(echo $ifaces | grep :$vid | jq '.id')
-      if [ $vid -le 230 ]; then
-        maas $MAAS_USER interface link-subnet $system_id $tiface mode=AUTO subnet=$subnet_id
-      fi
-    fi
-  done
-done
+system_id=$(maas telescreen machines read | jq --arg name $NAME '.[] | select(.hostname==$name) | .system_id' | tr -d '"')
+parent_interface_id=$(maas $MAAS_USER interfaces read $system_id | jq --arg INF $INF '.[] | select(.name == $INF)| .id')
+
+if [ $VLANID -ne 0 ]; then
+  tiface=$(maas $MAAS_USER interfaces create-vlan $system_id vlan=$vlan_id parent=$parent_interface_id | jq '.id')
+  maas $MAAS_USER interface link-subnet $system_id $tiface mode=AUTO subnet=$subnet_id
+fi
