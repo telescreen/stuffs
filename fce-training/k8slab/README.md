330f5560406f9fb1e571ba69b9524f19e9544b1e
telescreen <ha@buihanotes.com>
2020-03-07 18:38:27 +0900

    Add k8slab fce. Cleanup lxd settings

---
+++ fce-training/k8slab/README.md
@@ -0,0 +1,2 @@
+Nothing in master for now; this may become a base
+template for all other templates, though.
--- fce-training/testenv.sh
+++ fce-training/testenv.sh
@@ -4,34 +4,22 @@ clean_up () {
   # Stop old vms
   lxc stop maas
   lxc stop virtvm
-  lxc stop jujuws
-  #lxc stop proxy
   
-  lxc delete maas
   lxc delete virtvm
-  lxc delete jujuws
-  #lxc delete proxy
+  lxc delete maas
   
   # Delete old profiles
-  lxc profile delete maas
   lxc profile delete virtvm
-  #lxc profile delete proxy
 }
 
 create_new () {
   # Create new profile
-  lxc profile create maas
-  lxc profile edit maas < maas-profile.yaml
   lxc profile create virtvm
   lxc profile edit virtvm < virtvm-profile.yaml
-  #lxc profile create proxy
-  #lxc profile edit proxy < proxy-profile.yaml
 
   # Launch VMs
-  lxc launch ubuntu:18.04 -p maas maas --config=user.network-config="$(cat maas.yaml)"
+  lxc launch ubuntu:18.04 -p virtvm maas --config=user.network-config="$(cat maas.yaml)"
   lxc launch ubuntu:18.04 -p virtvm virtvm --config=user.network-config="$(cat virtvm.yaml)"
-  lxc launch ubuntu:18.04 -p maas jujuws --config=user.network-config="$(cat jujuws.yaml)"
-  #lxc launch ubuntu:18.04 -p proxy proxy --config=user.network-config="$(cat proxy.yaml)"
   lxc config device add maas maas5240 proxy connect=tcp:172.16.224.2:5240 listen=tcp:0.0.0.0:5240
 }
 
