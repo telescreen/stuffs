f6f90c4440e0a22c0c273bb63c201eb9fbf58740
Ha Bui <ha@buihanotes.com>
2023-05-18 17:07:36 +0900

    Add upload_drive script

---
+++ crawlers/upload_drive.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def upload_basic(name, mimetype, folder_id):
+    """Insert new file.
+    Returns : Id's of the file uploaded
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+
+        folder_metadata = {'id': folder_id}
+        file_metadata = {
+          'name': name,
+          'parents': [folder_id]
+        }
+        media = MediaFileUpload(name, mimetype=mimetype, resumable=True)
+        # pylint: disable=maybe-no-member
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        print(F'File ID: {file.get("id")}')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return file.get('id')
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    mimetype = sys.argv[2]
+    parent = sys.argv[3]
+    upload_basic(filename, mimetype, parent)
