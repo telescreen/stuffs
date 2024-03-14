751cfca121775d245adcbf0e3568af882b967c87
Ha Bui <ha@buihanotes.com>
2024-03-14 20:49:46 +0900

    Add many useful tools
    * wp.sh to automatically change wallpaper
    * Use C++17 by default in cppbuild
    * Refactor launch-instance.sh.

--- infra/launch-instance.sh
+++ infra/launch-instance.sh
@@ -10,15 +10,40 @@ INSTANCE_NAME=$1
 BASE_IMAGE=jammy-server-cloudimg-amd64.img
 CLOUD_INIT_IMG=cloud_init.img
 
+if [ ! -f ${BASE_IMAGE} ]
+then
+  wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+  qemu-img resize ${BASE_IMAGE} 20G
+fi
+
+### DISK ###
 if [ ! -f ${INSTANCE_NAME}.qcow2 ]
 then
   qemu-img create -f qcow2 -F qcow2 -b ${BASE_IMAGE} ${INSTANCE_NAME}.qcow2
 fi
 
-if [ ! -f ${CLOUD_INIT_IMG} ]
-then
-  cloud-localds ${CLOUD_INIT_IMG} cloud_init.cfg
-fi
+### CLOUD_INIT ###
+# password: possible
+cat > cloud_init.cfg <<EOF
+#cloud-config
+timezone: Asia/Tokyo
+locale: en_US.utf8
+package_upgrade: True
+hostname: ${INSTANCE_NAME}
+chpasswd:
+  expire: False
+users:
+  - name: ubuntu
+    ssh_import_id:
+      - gh:telescreen
+    lock_passwd: false
+    passwd: "\$6\$0SSkAaAjMmpvEAmH\$J.a1O3IhQ5EejvtALUdt9DUEtZ6K9eTq4ICClDAu6ngAeMMrmDK/hSfW3URZ92LjdD4MTnJ/yJmSn1oRwEZox/"
+    shell: /bin/bash
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    uid: 1000
+
+EOF
+cloud-localds ${CLOUD_INIT_IMG} cloud_init.cfg
 
 virt-install    \
   --name ${INSTANCE_NAME}  \
@@ -29,6 +54,7 @@ virt-install    \
   --disk path=$PWD/${INSTANCE_NAME}.qcow2    \
   --disk path=$PWD/${CLOUD_INIT_IMG},device=cdrom    \
   --network bridge=virbr0,model=virtio    \
-  --graphics none --serial pty --console pty    \
+  --serial pty \
+  --noautoconsole \
   --import
 
---
+++ wp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# script to set random background wallpapers on my GNOME desktop
+# set base path
+export wallpaper_path=/home/telescreen/Pictures/Wallpapers
+shopt -s nullglob
+# store all the image file names in wallpapers array
+wallpapers=(
+    "$wallpaper_path"/*.jpg
+    "$wallpaper_path"/*.jpeg
+    "$wallpaper_path"/*.png
+    "$wallpaper_path"/*.bmp
+    "$wallpaper_path"/*.svg
+)
+# get array size
+wallpapers_size=${#wallpapers[*]}
+
+# set wallpapers in incremental order
+index=0
+while [ $index -lt $wallpapers_size ]
+do
+    gsettings set org.gnome.desktop.background picture-uri "${wallpapers[$index]}"
+    # index is maxing out, so reset it
+    if [ $(($index+1)) -eq $wallpapers_size ]
+    then
+        index=0
+    else
+        index=$(($index + 1))
+    fi
+    # keep the wallpaper for the specified time
+    sleep 5m
+done
